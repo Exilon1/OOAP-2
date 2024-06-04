6b11b7eff97668182bd111a532a8466ccc950110
Aleksey Nikotin <exilonone@gmail.com>
2024-06-04 17:53:58 +0400

    Task 13 comment

--- src/inheritance13/FourA.java
+++ src/inheritance13/FourA.java
@@ -2,6 +2,7 @@ package inheritance13;
 
 public class FourA {
 
+  // protected вместо private, чтобы можно было сделать вызов класса B из другого класса
   protected void method() {
     System.out.println("Protected method in class FourA");
   }
