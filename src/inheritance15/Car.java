e78b0fa5f3a429d65bb76f9be947603762c97ba3
Aleksey Nikotin <exilonone@gmail.com>
2024-06-13 14:21:07 +0400

    Task 15

---
+++ src/inheritance15/Car.java
@@ -0,0 +1,12 @@
+package inheritance15;
+
+public class Car {
+}
+
+class AutomaticGearBoxCar extends Car {
+  // Характеристики автоматической коробки передач
+}
+
+class HybridEngineCar extends Car {
+  // Характеристики гибридного движка
+}
